2b9b804475abcec7d02a26cae510aa515d2e1bc1
hunter <hunter.li@haizhi.com>
2017-08-28 18:54:39 +0800

    修复不可用bug

--- src/index.js
+++ src/index.js
@@ -82,168 +82,7 @@ angular.module(moduleName).controller('mainCtrl', function ($scope, $http, $q, u
     };
 });
 
-/**
- * @param {Object} propList - 转换为展示数据时，每个节点所携带的数据属性，eg.{id: 'id',fullname: 'txt',level: 'level',parentIds: 'parentIds',isExpand: 'isExpand'}
- */
-angular.module(moduleName).directive('infiniteScrollbar', function ($timeout, DataMechanism, utilService) {
-    return {
-        restrict: 'A',
-        scope: {
-            dataList: '=',                          // 数据
-            viewList: '=',                      // 展示列表
-            toggleHandler: '=?',                // 展开收起属性结构方法
-            getChildNodeApi: '&?',              // 获取子列表数据api
-            isGetChildFromApi: '@',             // 是否通过api拉取子列表数据
-            itemHeight: '@',                    // 每个item的展示高度
-            showCount: '@',                     // 固定的展示数量
-            isTree: '@',                        // 是否为树形结构
-            propList: '=?'                      // 需要在每个node节点中绑定的属性，树形结构默认为[id, fullname, level, parentIds, isExpand]
-        },
-        link: function (scope, element, attr) {
-            // 属性结构必须为数组
-            if (!angular.isArray(scope.dataList)) {
-                console.trace('tree must be array');
-                return false;
-            }
 
-            // 节点高度必须为数字
-            if (!/[1-9]+/.test(scope.itemHeight)) {
-                console.trace('itemHeight must be number');
-                return false;
-            }
-
-            //设置默认项
-            (function () {
-                if (angular.isUndefined(scope.propList)) {
-                    scope.propList = {
-                        id: 'id',
-                        fullname: 'txt',
-                        level: 'level',
-                        parentIds: 'parentIds',
-                        isExpand: 'isExpand'
-                    }
-                }
-            })();
-
-            /**
-             * @description 计算当前可展示的item数量
-             * @returns {number}
-             * @private
-             */
-            function _calShowCount() {
-                if (scope.showCount && scope.showCount !== '0') {
-                    return Number(scope.showCount);
-                } else {
-                    return Math.ceil($(element).parent().height() / Number(scope.itemHeight) + 6);
-                }
-            }
-
-            function _setPlaceholderHeight(itemHeight) {
-                let _topH = itemHeight * dataMechanism.cursor.start;
-                let _bottomH = itemHeight * (dataMechanism.dataList.length - dataMechanism.viewList.length - dataMechanism.cursor.start);
-
-                $(element).parent().children().first().css('height', _topH);
-                $(element).parent().children().last().css('height', _bottomH);
-            }
-
-            let _time = new Date().getTime();                               // 获取当前时间，定义占位符Id
-            let _nodeDict = new Map();                                      // 数据字典
-            let dataMechanism = new DataMechanism(scope.viewList);          // 定义数据处理机制对象
-            let _itemH = Number(scope.itemHeight);                          // 单独一个节点的高度
-
-            $(element).parent().prepend(`<i id="${'up_p_' + _time + '_' + Math.ceil(Math.random() * 1000)}" style="display: block"></i>`);
-            $(element).parent().append(`<i id="${'down_p_' + _time + '_' + Math.ceil(Math.random() * 1000)}" style="display: block"></i>`);
-
-            function _viewChangeOperate() {
-                utilService.safeApply(scope, function () {
-                    dataMechanism.setViewData();
-                    _setPlaceholderHeight(Number(scope.itemHeight));
-                });
-            }
-
-            dataMechanism.setShowCount(_calShowCount());                    // 设置展示个数
-
-            if (scope.isTree) {
-                utilService.tree2Map(scope.dataList, _nodeDict, scope.propList);    // 将树形结构转换为Map
-                dataMechanism.dataList.push(_nodeDict.get(scope.dataList[0].id));   // 添加root节点
-                dataMechanism.dataList[0].childrenIds.forEach(id => dataMechanism.dataList.push(_nodeDict.get(id))); // 添加root节点的第一层子节点
-            } else {
-                dataMechanism.dataList = scope.dataList;
-            }
-            _viewChangeOperate();
-
-            $(element).parent().bind('scroll', function (event) {
-                if (dataMechanism.dataList.length > dataMechanism.showCount) {      // 若数据展示条数过少，则无需调整
-                    let _targetIdx = Math.floor(event.target.scrollTop / _itemH);       // 计算当前需要展示的索引
-
-                    if (Math.abs(_targetIdx - dataMechanism.cursor.start) > dataMechanism.redundancy - 1 || _targetIdx < dataMechanism.redundancy) {
-                        dataMechanism.cursor.start = _targetIdx - dataMechanism.redundancy > 0 ? _targetIdx - dataMechanism.redundancy : 0;
-                        _viewChangeOperate()
-                    }
-                }
-            });
-
-            /**
-             * @description 展开和收起子列表
-             * @param id
-             */
-            scope.toggleHandler = scope.isTree ? function (id) {
-                let _curNode = _nodeDict.get(id),    // 当前节点
-                    _currIdx = 0;                               // 当前节点在展示列表中的索引
-
-                //计算索引值
-                for (let i = 0; i < dataMechanism.dataList.length; i++) {
-                    if (id === dataMechanism.dataList[i].id) {
-                        _currIdx = i;
-                        break;
-                    }
-                }
-
-                if (_curNode) {
-                    if (!_curNode.isExpand) { // 节点未打开时
-                        _curNode.isExpand = true;
-
-                        if (scope.isGetChildFromApi) {
-                            scope.getChildNodeApi({id: id}).then(function (result) {
-                                let _tempArr = [];
-                                result.children.forEach(item => _tempArr.push(item));
-                                $timeout(function () {
-                                    utilService.insertArrToArr.apply(dataMechanism.dataList, [_currIdx, _tempArr]);
-                                    _viewChangeOperate()
-                                });
-                            });
-                        } else {
-                            if (_curNode.childrenIds) {
-                                let _tempArr = [];
-                                _curNode.childrenIds.forEach(id => _tempArr.push(_nodeDict.get(id)));
-                                utilService.insertArrToArr.apply(dataMechanism.dataList, [_currIdx, _tempArr]);
-                                _viewChangeOperate();
-                            }
-                        }
-                    } else { // 节点已经打开
-                        _curNode.isExpand = false;
-
-                        let _spliceLen = 0;
-                        for (let i = _currIdx + 1; i < dataMechanism.dataList.length; i++) {
-                            let _tempItem = dataMechanism.dataList[i];
-                            if (_tempItem && _tempItem.level === _curNode.level) {
-                                break;
-                            }
-
-                            if (_tempItem && _tempItem.level > _curNode.level) {
-                                _tempItem.isExpand = false;
-                                _spliceLen++;
-                            }
-                        }
-
-                        dataMechanism.dataList.splice(_currIdx + 1, _spliceLen);
-                        _viewChangeOperate();
-                    }
-                }
-            } : null;
-        }
-    }
-});
 angular.module(moduleName).service('utilService', function () {
     /**
      * @description 将数组arr在指定位置插入到调用数组中，调用时需要使用apply进行调用
@@ -334,6 +173,172 @@ angular.module(moduleName).factory('DataMechanism', function () {
 
     return DataMechanism;
 });
+
+/**
+ * @param {Object} propList - 转换为展示数据时，每个节点所携带的数据属性，eg.{id: 'id',fullname: 'txt',level: 'level',parentIds: 'parentIds',isExpand: 'isExpand'}
+ */
+angular.module(moduleName).directive('infiniteScrollbar', ['$timeout', 'DataMechanism', '$injector',
+    function ($timeout, DataMechanism, $injector) {
+        return {
+            restrict: 'A',
+            scope: {
+                dataList: '=',                          // 数据
+                viewList: '=',                      // 展示列表
+                toggleHandler: '=?',                // 展开收起属性结构方法
+                getChildNodeApi: '&?',              // 获取子列表数据api
+                isGetChildFromApi: '@',             // 是否通过api拉取子列表数据
+                itemHeight: '@',                    // 每个item的展示高度
+                showCount: '@',                     // 固定的展示数量
+                isTree: '@',                        // 是否为树形结构
+                propList: '=?'                      // 需要在每个node节点中绑定的属性，树形结构默认为[id, fullname, level, parentIds, isExpand]
+            },
+            link: function (scope, element, attr) {
+                // 属性结构必须为数组
+                if (!angular.isArray(scope.dataList)) {
+                    console.trace('tree must be array');
+                    return false;
+                }
+
+                // 节点高度必须为数字
+                if (!/[1-9]+/.test(scope.itemHeight)) {
+                    console.trace('itemHeight must be number');
+                    return false;
+                }
+
+                let utilService = $injector.get('utilService');
+
+                //设置默认项
+                (function () {
+                    if (angular.isUndefined(scope.propList)) {
+                        scope.propList = {
+                            id: 'id',
+                            fullname: 'txt',
+                            level: 'level',
+                            parentIds: 'parentIds',
+                            isExpand: 'isExpand'
+                        }
+                    }
+                })();
+
+                /**
+                 * @description 计算当前可展示的item数量
+                 * @returns {number}
+                 * @private
+                 */
+                function _calShowCount() {
+                    if (scope.showCount && scope.showCount !== '0') {
+                        return Number(scope.showCount);
+                    } else {
+                        return Math.ceil($(element).parent().height() / Number(scope.itemHeight) + 6);
+                    }
+                }
+
+                function _setPlaceholderHeight(itemHeight) {
+                    let _topH = itemHeight * dataMechanism.cursor.start;
+                    let _bottomH = itemHeight * (dataMechanism.dataList.length - dataMechanism.viewList.length - dataMechanism.cursor.start);
+
+                    $(element).parent().children().first().css('height', _topH);
+                    $(element).parent().children().last().css('height', _bottomH);
+                }
+
+                let _time = new Date().getTime();                               // 获取当前时间，定义占位符Id
+                let _nodeDict = new Map();                                      // 数据字典
+                let dataMechanism = new DataMechanism(scope.viewList);          // 定义数据处理机制对象
+                let _itemH = Number(scope.itemHeight);                          // 单独一个节点的高度
+
+                $(element).parent().prepend(`<i id="${'up_p_' + _time + '_' + Math.ceil(Math.random() * 1000)}" style="display: block"></i>`);
+                $(element).parent().append(`<i id="${'down_p_' + _time + '_' + Math.ceil(Math.random() * 1000)}" style="display: block"></i>`);
+
+                function _viewChangeOperate() {
+                    utilService.safeApply(scope, function () {
+                        dataMechanism.setViewData();
+                        _setPlaceholderHeight(Number(scope.itemHeight));
+                    });
+                }
+
+                dataMechanism.setShowCount(_calShowCount());                    // 设置展示个数
+
+                if (scope.isTree) {
+                    utilService.tree2Map(scope.dataList, _nodeDict, scope.propList);    // 将树形结构转换为Map
+                    dataMechanism.dataList.push(_nodeDict.get(scope.dataList[0].id));   // 添加root节点
+                    dataMechanism.dataList[0].childrenIds.forEach(id => dataMechanism.dataList.push(_nodeDict.get(id))); // 添加root节点的第一层子节点
+                } else {
+                    dataMechanism.dataList = scope.dataList;
+                }
+                _viewChangeOperate();
+
+                $(element).parent().bind('scroll', function (event) {
+                    if (dataMechanism.dataList.length > dataMechanism.showCount) {      // 若数据展示条数过少，则无需调整
+                        let _targetIdx = Math.floor(event.target.scrollTop / _itemH);       // 计算当前需要展示的索引
+
+                        if (Math.abs(_targetIdx - dataMechanism.cursor.start) > dataMechanism.redundancy - 1 || _targetIdx < dataMechanism.redundancy) {
+                            dataMechanism.cursor.start = _targetIdx - dataMechanism.redundancy > 0 ? _targetIdx - dataMechanism.redundancy : 0;
+                            _viewChangeOperate()
+                        }
+                    }
+                });
+
+                /**
+                 * @description 展开和收起子列表
+                 * @param id
+                 */
+                scope.toggleHandler = scope.isTree ? function (id) {
+                    let _curNode = _nodeDict.get(id),    // 当前节点
+                        _currIdx = 0;                               // 当前节点在展示列表中的索引
+
+                    //计算索引值
+                    for (let i = 0; i < dataMechanism.dataList.length; i++) {
+                        if (id === dataMechanism.dataList[i].id) {
+                            _currIdx = i;
+                            break;
+                        }
+                    }
+
+                    if (_curNode) {
+                        if (!_curNode.isExpand) { // 节点未打开时
+                            _curNode.isExpand = true;
+
+                            if (scope.isGetChildFromApi) {
+                                scope.getChildNodeApi({id: id}).then(function (result) {
+                                    let _tempArr = [];
+                                    result.children.forEach(item => _tempArr.push(item));
+                                    $timeout(function () {
+                                        utilService.insertArrToArr.apply(dataMechanism.dataList, [_currIdx, _tempArr]);
+                                        _viewChangeOperate()
+                                    });
+                                });
+                            } else {
+                                if (_curNode.childrenIds) {
+                                    let _tempArr = [];
+                                    _curNode.childrenIds.forEach(id => _tempArr.push(_nodeDict.get(id)));
+                                    utilService.insertArrToArr.apply(dataMechanism.dataList, [_currIdx, _tempArr]);
+                                    _viewChangeOperate();
+                                }
+                            }
+                        } else { // 节点已经打开
+                            _curNode.isExpand = false;
+
+                            let _spliceLen = 0;
+                            for (let i = _currIdx + 1; i < dataMechanism.dataList.length; i++) {
+                                let _tempItem = dataMechanism.dataList[i];
+                                if (_tempItem && _tempItem.level === _curNode.level) {
+                                    break;
+                                }
+
+                                if (_tempItem && _tempItem.level > _curNode.level) {
+                                    _tempItem.isExpand = false;
+                                    _spliceLen++;
+                                }
+                            }
+
+                            dataMechanism.dataList.splice(_currIdx + 1, _spliceLen);
+                            _viewChangeOperate();
+                        }
+                    }
+                } : null;
+            }
+        }
+    }]);
 
 
 
